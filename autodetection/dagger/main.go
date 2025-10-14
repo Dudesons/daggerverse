e6a3bb0401050e7f9aeeee50e58e2bf976f08db4
Dudesons <damdam.gold@gmail.com>
2025-10-14 11:26:02 +0200

    fix

--- autodetection/dagger/main.go
+++ autodetection/dagger/main.go
@@ -9,9 +9,7 @@ import (
 
 type Autodetection struct {
 	// Used to overwrite the default image used for internal action (mainly used to avoid rate limit with dockerhub)
-	// +optional
-	// +default="alpine:latest"
-	InternalImage string
+
 }
 
 // Expose node auto dection runtime information
@@ -19,11 +17,14 @@ func (a *Autodetection) Node(
 	ctx context.Context,
 	// The path to the project to analyze
 	src *dagger.Directory,
+	// +optional
+	// +default="alpine:latest"
+	internalImage string,
 	// Define patterns to exclude from the analysis
 	// +optional
 	patternExclusions []string,
 ) (*NodeAnalyzer, error) {
-	return newNodeAnalyzer(ctx, src, patternExclusions, a.InternalImage)
+	return newNodeAnalyzer(ctx, src, patternExclusions, internalImage)
 }
 
 // Expose OCI dection runtime information
@@ -31,9 +32,12 @@ func (a *Autodetection) Oci(
 	ctx context.Context,
 	// The path to the project to analyze
 	src *dagger.Directory,
+	// +optional
+	// +default="alpine:latest"
+	internalImage string,
 	// Define patterns to exclude from the analysis
 	// +optional
 	patternExclusions []string,
 ) (*OciAnalyzer, error) {
-	return newOciAnalyzer(ctx, src, patternExclusions, a.InternalImage)
+	return newOciAnalyzer(ctx, src, patternExclusions, internalImage)
 }
